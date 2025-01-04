5dda61d3fa36eec87b52d9955f4d1cc3dc7d2325
hasithajayasundara <hasithajayasundara1@gmail.com>
2025-01-04 18:42:41 +0100

    cleanup

--- product-list-with-cart/src/js/controller.js
+++ product-list-with-cart/src/js/controller.js
@@ -1,10 +1,5 @@
 import { data } from './mocks/data';
-import { state } from './model';
-import { mapProducts } from './helpers';
-import {
-  CART_OPERATION_DECREMENT,
-  CART_OPERATION_INCREMENT
-} from './constants';
+import * as model from './model';
 
 import productView from './views/productView';
 import cartView from './views/cartView';
@@ -12,8 +7,7 @@ import orderConfirmedView from './views/orderConfirmedView';
 
 export const getProducts = async () => new Promise((resolve) => {
   setTimeout(() => {
-    const { productMap, productIds } = mapProducts(data);
-    state.products = { byId: productMap, allIds: productIds };
+    model.setProducts(data);
     resolve();
   }, 2000);
 });
@@ -27,94 +21,41 @@ const controlProducts = async () => {
     await getProducts();
 
     // Render product view
-    productView.render(state);
+    productView.render(model.state);
 
     // Render cart view
-    cartView.render(state);
+    cartView.render(model.state);
   } catch (err) {
     console.log(err);
   }
 };
 
 const controlAddToCart = (id) => {
-  const cartItemsById = state.cart.items.byId;
-  if (cartItemsById.has(id)) {
-    const quantity = cartItemsById.get(id);
-    cartItemsById.set(id, quantity + 1);
-  } else {
-    state.cart.items.allIds.push(id);
-    cartItemsById.set(id, 1);
-  }
-
-  // Calculate total price
-  const product = state.products.byId.get(id);
-  const quantity = cartItemsById.get(id);
-  state.cart.totalItems += 1;
-  state.cart.totalPrice += (product.price * quantity);
-
-  productView.render(state);
-  cartView.render(state);
+  model.addToCart(id);
+  productView.render(model.state);
+  cartView.render(model.state);
 };
 
 const controlChangeCart = (id, operation) => {
-  const cartItemsById = state.cart.items.byId;
-  const product = state.products.byId.get(id);
-
-  if (cartItemsById.has(id)) {
-    const quantity = cartItemsById.get(id);
-    if (operation === CART_OPERATION_DECREMENT) {
-      cartItemsById.set(id, quantity - 1);
-      state.cart.totalItems -= 1;
-      state.cart.totalPrice -= product.price;
-    } else if (operation === CART_OPERATION_INCREMENT) {
-      cartItemsById.set(id, quantity + 1);
-      state.cart.totalItems += 1;
-      state.cart.totalPrice += product.price;
-    }
-
-    // After updating if the quantity is 0 remove it from cart items map
-    if (cartItemsById.get(id) === 0) {
-      cartItemsById.delete(id);
-      state.cart.items.allIds = state.cart.items.allIds.filter((i) => i !== id);
-    }
-
-    productView.render(state);
-    cartView.render(state);
-  }
+  model.changeCart(id, operation);
+  productView.render(model.state);
+  cartView.render(model.state);
 };
 
 const controlRemoveCartItem = (id) => {
-  const cartItemsById = state.cart.items.byId;
-  if (cartItemsById.has(id)) {
-    const product = state.products.byId.get(id);
-    const quantity = cartItemsById.get(id);
-
-    // Remove item from cart
-    cartItemsById.delete(id);
-    state.cart.items.allIds = state.cart.items.allIds.filter((i) => i !== id);
-
-    // Modify total price
-    state.cart.totalPrice -= (quantity * product.price);
-
-    // Modify items count
-    state.cart.totalItems -= quantity;
-
-    productView.render(state);
-    cartView.render(state);
-  }
+  model.removeCartItem(id);
+  productView.render(model.state);
+  cartView.render(model.state);
 };
 
 const controlConfirmOrder = () => {
-  orderConfirmedView.render(state);
+  orderConfirmedView.render(model.state);
 };
 
 const controlStartNewOrder = () => {
-  state.cart.totalPrice = 0;
-  state.cart.totalItems = 0;
-  state.cart.items.allIds = [];
-  state.cart.items.byId = new Map();
-  productView.render(state);
-  cartView.render(state);
+  model.resetState();
+  productView.render(model.state);
+  cartView.render(model.state);
 };
 
 const init = () => {
--- product-list-with-cart/src/js/model.js
+++ product-list-with-cart/src/js/model.js
@@ -1,3 +1,6 @@
+import { CART_OPERATION_DECREMENT, CART_OPERATION_INCREMENT } from "./constants";
+import { mapProducts } from "./helpers";
+
 export const state = {
   products: {
     byId: new Map(),
@@ -11,4 +14,75 @@ export const state = {
       allIds: [],
     },
   },
+};
+
+export const setProducts = (data) => {
+  const { productMap, productIds } = mapProducts(data);
+  state.products = { byId: productMap, allIds: productIds };
+};
+
+export const addToCart = (id) => {
+  const cartItemsById = state.cart.items.byId;
+  if (cartItemsById.has(id)) {
+    const quantity = cartItemsById.get(id);
+    cartItemsById.set(id, quantity + 1);
+  } else {
+    state.cart.items.allIds.push(id);
+    cartItemsById.set(id, 1);
+  }
+
+  // Calculate total price
+  const product = state.products.byId.get(id);
+  const quantity = cartItemsById.get(id);
+  state.cart.totalItems += 1;
+  state.cart.totalPrice += (product.price * quantity);
+};
+
+export const changeCart = (id, operation) => {
+  const cartItemsById = state.cart.items.byId;
+  const product = state.products.byId.get(id);
+
+  if (cartItemsById.has(id)) {
+    const quantity = cartItemsById.get(id);
+    if (operation === CART_OPERATION_DECREMENT) {
+      cartItemsById.set(id, quantity - 1);
+      state.cart.totalItems -= 1;
+      state.cart.totalPrice -= product.price;
+    } else if (operation === CART_OPERATION_INCREMENT) {
+      cartItemsById.set(id, quantity + 1);
+      state.cart.totalItems += 1;
+      state.cart.totalPrice += product.price;
+    }
+
+    // After updating if the quantity is 0 remove it from cart items map
+    if (cartItemsById.get(id) === 0) {
+      cartItemsById.delete(id);
+      state.cart.items.allIds = state.cart.items.allIds.filter((i) => i !== id);
+    }
+  }
+};
+
+export const removeCartItem = (id) => {
+  const cartItemsById = state.cart.items.byId;
+  if (cartItemsById.has(id)) {
+    const product = state.products.byId.get(id);
+    const quantity = cartItemsById.get(id);
+
+    // Remove item from cart
+    cartItemsById.delete(id);
+    state.cart.items.allIds = state.cart.items.allIds.filter((i) => i !== id);
+
+    // Modify total price
+    state.cart.totalPrice -= (quantity * product.price);
+
+    // Modify items count
+    state.cart.totalItems -= quantity;
+  }
+};
+
+export const resetState = () => {
+  state.cart.totalPrice = 0;
+  state.cart.totalItems = 0;
+  state.cart.items.allIds = [];
+  state.cart.items.byId = new Map();
 };
