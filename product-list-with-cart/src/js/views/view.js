eb553bcce6d007bef338db555bcedd9baa91bb09
hasithajayasundara <hasithajayasundara1@gmail.com>
2025-01-05 22:12:04 +0100

    cleanup

--- product-list-with-cart/src/js/views/view.js
+++ product-list-with-cart/src/js/views/view.js
@@ -1,5 +1,3 @@
-import DOMPurify from 'isomorphic-dompurify';
-
 class View {
   _data;
 
@@ -11,7 +9,7 @@ class View {
     this._parent.classList.add('loading');
     const markup = this._generateLoadingMarkup?.();
     this._clear();
-    this._parent.insertAdjacentHTML('afterbegin', DOMPurify.sanitize(markup ?? ''));
+    this._parent.insertAdjacentHTML('afterbegin', markup);
   }
 
   render(data) {
@@ -19,12 +17,7 @@ class View {
     const markup = this._generateMarkup?.();
     this._clear();
     this._parent.classList.remove('loading');
-    this._parent.insertAdjacentHTML('afterbegin', DOMPurify.sanitize(markup ?? ''));
-  }
-
-  renderSpinner() {
-    this._clear();
-    this._parent.insertAdjacentHTML('afterbegin', '<div class="loader"></div>')
+    this._parent.insertAdjacentHTML('afterbegin', markup);
   }
 }
 
