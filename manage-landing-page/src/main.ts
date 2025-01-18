a914ffd736e5331da5fd1fbc192c0b1e7a502a0c
hasithajayasundara <hasithajayasundara1@gmail.com>
2025-01-18 08:10:26 +0100

    add basic slider

--- manage-landing-page/src/main.ts
+++ manage-landing-page/src/main.ts
@@ -1,11 +1,15 @@
+import A11YSlider from 'a11y-slider';
+
 const primaryHeader = document.querySelector('.primary-header');
 const navToggle = document.querySelector('.mobile-nav-toggle');
 const primaryNav = document.querySelector('.primary-navigation');
 
+const slider = new A11YSlider(document.querySelector('.slider')!, {});
+
 navToggle?.addEventListener('click', () => {
-  if(primaryNav?.hasAttribute('data-visible')){
+  if (primaryNav?.hasAttribute('data-visible')) {
     navToggle.setAttribute('aria-expanded', String(false));
-  }else{
+  } else {
     navToggle.setAttribute('aria-expanded', String(true));
   }
   primaryNav?.toggleAttribute('data-visible');
